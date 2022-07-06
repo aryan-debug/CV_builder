9f5a54f055c6fa0e2798a91939cde32758a812ae
Aryan <aryanarora2285@gmail.com>
2022-07-05 19:01:28 -0700

    fixing hosting issue

--- src/components/CVPreview.js
+++ src/components/CVPreview.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import AchievementView from "./CVPreviewComponents/AchievementView";
 import PersonalInfoView from "./CVPreviewComponents/PeronalInfoView";
 import ProjectsView from "./CVPreviewComponents/ProjectsView";
